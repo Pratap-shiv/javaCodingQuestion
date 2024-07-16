c22ac94cdb67920ae421071d4bbc247ef23c9250
Shiv Pratap <shiv.pratap@velocis.in>
2024-07-16 09:45:04 +0530

    initial commit

---
+++ src/test/java/com/example/javaCodingQuestion/JavaCodingQuestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaCodingQuestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodingQuestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
